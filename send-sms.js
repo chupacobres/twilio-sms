2bca516e3885ce481b4d5f47d3b4b447909728a1
juan uribe <juanpablouribe@mac.com>
2020-06-07 15:47:00 -0400

    added server.js to answer an SMS sent to my Twilio number with a specific message

--- send-sms.js
+++ send-sms.js
@@ -1,4 +1,6 @@
-// Add your credentials as environment variables in a twilio.env file and source them:
+// Add your credentials and destination numbers as environment variables 
+// in a twilio.env file and source them by typing source ./twilio.env 
+// on the command line:
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
@@ -6,8 +8,10 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 client.messages.create({
-    to: '+16466376625',
+    to: process.env.MY_PHONE_NUMBER,
     from: '+14807256363',
     body: 'Here we go again'
 })
-.then((message) => console.log(message.sid))
+    .then((message) => console.log(message.sid))
+
+
