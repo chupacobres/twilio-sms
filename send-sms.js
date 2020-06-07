a828deeda28941aaf2d96bb16f89990613f4cf0f
juan uribe <juanpablouribe@mac.com>
2020-06-07 12:46:33 -0400

    added credentials to env file and have them ignored on repository

--- send-sms.js
+++ send-sms.js
@@ -1,16 +1,13 @@
-const accountSid = 'ACb6a98650679a747cfd7fc8e3db5ea6b9';
-const authToken = '4d68999dc38ab9e2b7d28e67f588cf63';
+// Add your credentials as environment variables in a twilio.env file and source them:
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
-// process.env.TWILIO_ACCOUNT_SI = 'ACb6a98650679a747cfd7fc8e3db5ea6b9';
-// process.env.TWILIO_AUTH_TOKEN = '4d68999dc38ab9e2b7d28e67f588cf63';
-// process.env.MY_PHONE_NUMBER = '+16466376625';
-console.log(process.env);
 
 const client = require('twilio')(accountSid, authToken);
 
 client.messages.create({
     to: '+16466376625',
     from: '+14807256363',
-    body: 'Here we go'
+    body: 'Here we go again'
 })
 .then((message) => console.log(message.sid))
